df99d5eb6fb74fe366682f371b46929385635d79
do4x <aerianfyt@gmail.com>
2026-03-29 12:20:36 +0300

    .

--- constants/avatars.ts
+++ constants/avatars.ts
@@ -7,16 +7,16 @@ export interface Avatar {
 }
 
 export const AVATARS: Avatar[] = [
-  { id: "penguin1",  image: require("../assets/avatars/penguin1.png"),  label: "FROSTY" },
-  { id: "penguin2",  image: require("../assets/avatars/penguin2.png"),  label: "WADDLE" },
-  { id: "penguin3",  image: require("../assets/avatars/penguin3.png"),  label: "SLICK" },
-  { id: "penguin4",  image: require("../assets/avatars/penguin4.png"),  label: "CHILL" },
-  { id: "penguin5",  image: require("../assets/avatars/penguin5.png"),  label: "DRIFT" },
-  { id: "penguin6",  image: require("../assets/avatars/penguin6.png"),  label: "TUXEDO" },
-  { id: "penguin7",  image: require("../assets/avatars/penguin7.png"),  label: "BLIZZARD" },
-  { id: "penguin8",  image: require("../assets/avatars/penguin8.png"),  label: "FLIPPER" },
-  { id: "penguin9",  image: require("../assets/avatars/penguin9.png"),  label: "SHADOW" },
-  { id: "penguin10", image: require("../assets/avatars/penguin10.png"), label: "SPARKY" },
+  { id: "penguin1",  image: require("../assets/pfps/img1.jpg"),  label: "FROSTY" },
+  { id: "penguin2",  image: require("../assets/pfps/img2.jpg"),  label: "WADDLE" },
+  { id: "penguin3",  image: require("../assets/pfps/img3.png"),  label: "SLICK" },
+  { id: "penguin4",  image: require("../assets/pfps/img4.png"),  label: "CHILL" },
+  { id: "penguin5",  image: require("../assets/pfps/img5.png"),  label: "DRIFT" },
+  { id: "penguin6",  image: require("../assets/pfps/img6.jpg"),  label: "TUXEDO" },
+  { id: "penguin7",  image: require("../assets/pfps/img7.jpg"),  label: "BLIZZARD" },
+  { id: "penguin8",  image: require("../assets/pfps/img8.jpg"),  label: "FLIPPER" },
+  { id: "penguin9",  image: require("../assets/pfps/img9.jpg"),  label: "SHADOW" },
+  { id: "penguin10", image: require("../assets/pfps/img10.jpg"), label: "SPARKY" },
 ];
 
 export function getAvatar(id: string | null | undefined): Avatar {
